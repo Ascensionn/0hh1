e0090877424aa6fcc36044818f5fb1453b127b60
Ascensionn <87586499+Ascensionn@users.noreply.github.com>
2022-10-24 15:14:41 -0400

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -8,31 +8,6 @@ void startTests();
 
 int main()
 {
-
-    cout << "-------------------------------" << endl
-         << "EECS 183 Project 3 Menu Options" << endl
-         << "-------------------------------" << endl;
-    cout << "1) Execute testing functions in test.cpp" << endl;
-    cout << "2) Execute ohhi() function in main.cpp" << endl;
-    cout << "Choice --> ";
-
-    int choice;
-    cin >> choice;
-
-    // remove the return character from the cin stream buffer
-    string junk;
-    getline(cin, junk);
-
-    cout << endl;
-
-    if (choice == 1)
-    {
-        startTests();
-    }
-    else if (choice == 2)
-    {
-        ohhi();
-    }
-
+    ohhi();
     return 0;
 }
